cdd363741d677e1f785b0c1b9d568d1c23c0056c
Jacky Zhen <jacky.s.zhen@gmail.com>
2018-08-29 23:07:54 +1200

    Added a simple resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,10 @@ module.exports = {
       {
         label: 'About',
         path: '/about/'
+      },
+      {
+        label: 'Resume',
+        path: '/resume.pdf'
       }
     ],
     author: {

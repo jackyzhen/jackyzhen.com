08dabaf0a85f437d5293d8069adaa6c0458e29e6
Jacky Zhen <jacky.s.zhen@gmail.com>
2019-08-20 16:09:55 +1200

    add okapi analytics

--- src/html.js
+++ src/html.js
@@ -23,26 +23,43 @@ module.exports = class HTML extends React.Component {
     }
     return (
       <html {...this.props.htmlAttributes}>
-        <head>
-          <link rel="shortcut icon" href={faviconICO} />
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          {this.props.headComponents}
-          {css}
-        </head>
-        <body {...this.props.bodyAttributes}>
-          {this.props.preBodyComponents}
-          <div
-            key={`body`}
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
-          />
-          {this.props.postBodyComponents}
-        </body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function (o, k, a, p, i) {
+            o[p] = o[p] || function ()
+            { (o[p].queue = o[p].queue || []).push(arguments); }; var e = k.createElement(i);
+            var firstScript = k.getElementsByTagName(i)[0]; e.async = 1; e.src = a;
+            firstScript.parentNode.insertBefore(e, firstScript);
+          })(window, document,
+          '//okapianalytics.com/zeug.js',
+          'okapi', 'script');
+          okapi('set', 'tokenId', 'b91490a5-0325-43f6-9d9a-c547ee69b4af');
+          okapi('set', 'trackerApiUrl', '//okapianalytics.com/api/collect' );
+          okapi('trackPageView');
+            `}}
+        />
+</script>
+<!-- End Okapi Analytics -->
+<head>
+  <link rel="shortcut icon" href={faviconICO} />
+  <meta charSet="utf-8" />
+  <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+  />
+  {this.props.headComponents}
+  {css}
+</head>
+<body {...this.props.bodyAttributes}>
+  {this.props.preBodyComponents}
+  <div
+    key={`body`}
+    id="___gatsby"
+    dangerouslySetInnerHTML={{ __html: this.props.body }}
+  />
+  {this.props.postBodyComponents}
+</body>
       </html>
     )
   }
